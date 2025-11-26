941a8dd11c7fc93602a7534a07e4c8c7c1b25c6a
nathan-www <accounts@nathanarnold.co.uk>
2025-11-26 17:05:55 +0000

    test

--- config/wireguard/create_default_interface.bash
+++ config/wireguard/create_default_interface.bash
@@ -8,26 +8,28 @@ WIREGUARD_PORTAL_ADMIN_API_TOKEN=$(grep '^WIREGUARD_PORTAL_ADMIN_API_TOKEN=' "$E
 INTERFACE_PRIVATE_KEY=$(docker exec -u root wireguard-vpn wg genkey)
 INTERFACE_PUBLIC_KEY=$(echo "$INTERFACE_PRIVATE_KEY" | docker exec -i -u root wireguard-vpn wg pubkey)
 
-
-INTERFACE_JSON=$(cat <<EOF
+docker exec -i wireguard-vpn curl -s -X POST \
+  -u "admin:$WIREGUARD_PORTAL_ADMIN_API_TOKEN" \
+  -H "Content-Type: application/json" \
+  -d @- \
+  http://localhost:8888/api/v1/interface/new <<EOF
 {
   "Identifier": "wg0",
   "DisplayName": "wg0",
   "Mode": "server",
   "PrivateKey": "$INTERFACE_PRIVATE_KEY",
   "PublicKey": "$INTERFACE_PUBLIC_KEY",
-  "Addresses": ["10.255.0.2/32"],
+  "Addresses": [
+    "10.255.0.2/32"
+  ],
   "ListenPort": 51820,
   "PreUp": "iptables -t nat -A POSTROUTING -d 10.8.0.0/24 -j MASQUERADE",
   "PeerDefEndpoint": "49.13.87.41:51820",
-  "PeerDefNetwork": "10.255.0.0/24",
-  "PeerDefAllowedIPs": "10.8.0.0/24"
+  "PeerDefNetwork": [
+    "10.255.0.0/24"
+  ],
+  "PeerDefAllowedIPs": [
+    "10.8.0.0/24"
+  ]
 }
 EOF
-)
-
-docker exec -i wireguard-vpn curl -X POST \
-  -u "admin-service-user:$WIREGUARD_PORTAL_ADMIN_API_TOKEN" \
-  -H "Content-Type: application/json" \
-  -d "$JSON_PAYLOAD" \
-  http://localhost:8888/api/v1/interface/new

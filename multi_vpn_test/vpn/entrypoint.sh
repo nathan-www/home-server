656062994cf5c8dda41da7a0baed7f5d5219fef3
nathan-www <accounts@nathanarnold.co.uk>
2025-11-30 13:00:57 +0000

    test

--- multi_vpn_test/vpn/entrypoint.sh
+++ multi_vpn_test/vpn/entrypoint.sh
@@ -7,15 +7,6 @@ if [ ! -f /etc/wireguard/wg0.conf ]; then
     exit 1
 fi
 
-# Check if wg-second-hop config exists
-if [ ! -f /etc/wireguard/wg-second-hop.conf ]; then
-    echo "wg-second-hop.conf not found in /etc/wireguard!"
-    exit 1
-fi
-
-# Bring up second hop interface first
-# wg-quick up wg-second-hop
-
 # Bring up server (wg0) next
 wg-quick up wg0
 
